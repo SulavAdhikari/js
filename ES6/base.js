c18c206aca4044ae6676c46cde7a7f2b5961c8d9
sulavadhikari <cabbage.adhikari18@gmail.com>
2023-09-01 16:54:31 +0545

    commit

--- ES6/base.js
+++ ES6/base.js
@@ -11,3 +11,7 @@ const companies = [ { name: "Company One", category: "Finance", start: 1981, end
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 const person = { name: "Costas", address: { street: "Lalaland 12" } };
+
+
+
+
